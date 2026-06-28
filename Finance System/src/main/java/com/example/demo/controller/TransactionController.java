c377717ff6972ab76346ceea1848b41c8160bdce
nicholas-amarante <nicholasfelipe361@gmail.com>
2026-06-27 21:48:05 -0300

    remoção dos .class

--- Finance System/src/main/java/com/example/demo/controller/TransactionController.java
+++ Finance System/src/main/java/com/example/demo/controller/TransactionController.java
@@ -2,13 +2,18 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.CreateTransactionDTO;
 import com.example.demo.dto.DashboardDTO;
+import com.example.demo.dto.PagedDTO;
 import com.example.demo.dto.TransactionDTO;
+import com.example.demo.models.User;
 import com.example.demo.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -23,5 +28,20 @@ public class TransactionController {
     public ResponseEntity<Void> createTransaction(@RequestBody CreateTransactionDTO createTransactionDTO){
         transactionService.createTransaction(createTransactionDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedDTO.PagedResponse<TransactionDTO.TransactionFeed>> getTransaction(
+            @AuthenticationPrincipal User loggedUser,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) String type,
+            @RequestParam(required = false) Long accountId,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        var response=transactionService.getFilteredTransaction(loggedUser, description, type, accountId, startDate, endDate, page, size);
+        return ResponseEntity.ok(response);
     }
 }
