22cb4a96f33107b614ced4ebb07a40e184054817
nicholas-amarante <nicholasfelipe361@gmail.com>
2026-08-31 21:39:28 -0300

    refactor: move arquivos do projeto para a raiz do repositorio

--- Finance System/src/main/java/com/example/demo/dto/ResponseCategoryDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record ResponseCategoryDTO(
-        @NotNull(message = "O nome não pode ser nulo para categoria!")
-        String name
-) {
-}
